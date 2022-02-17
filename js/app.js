f41ad2f9dd5caa47eb7939cb006f7eef121dd59d
shufol islam <shufolislam1255@gmail.com>
2022-02-17 23:52:02 +0600

    almost done

--- js/app.js
+++ js/app.js
@@ -1,73 +1,76 @@
 function commonFunction(){
     // get income
-const incomeInput = document.getElementById('income-input').value;
-const incomeNumber = parseFloat(incomeInput);
-
+    const incomeInput = document.getElementById('income-input');
+    const incomeNumber = parseFloat(incomeInput.value);
+ 
     // get expenses
-    const foodInput = document.getElementById('food-input').value;
-    const foodInputNumber = parseFloat(foodInput)
-    const rentInput = document.getElementById('rent-input').value;
-    const rentInputNumber = parseFloat(rentInput)
-    const clothsInput = document.getElementById('cloths-input').value;
-    const clothsInputNumber = parseFloat(clothsInput)
-
+    const foodInput = document.getElementById('food-input');
+    const foodInputNumber = parseFloat(foodInput.value)
+    const rentInput = document.getElementById('rent-input');
+    const rentInputNumber = parseFloat(rentInput.value)
+    const clothsInput = document.getElementById('cloths-input');
+    const clothsInputNumber = parseFloat(clothsInput.value)
+ 
+    if(incomeInput.value == '' || isNaN(incomeInput.value) || incomeInput.value < 0){
+        alert('Income input value is wrong.');
+        return;
+    }
+    if(foodInput.value == '' || isNaN(foodInput.value) || foodInput.value < 0){
+        alert('Food input value is wrong.');
+        return;
+    }
+    if(rentInput.value == '' || isNaN(rentInput.value) || rentInput.value < 0){
+        alert('Rent input value is wrong.');
+        return;
+    }
+    if(clothsInput.value == '' || isNaN(clothsInput.value) || clothsInput.value < 0){
+        alert('Cloths input value is wrong.');
+        return;
+    }
+ 
     let expenseTotal = foodInputNumber + rentInputNumber + clothsInputNumber;
-
-        // calculating total expense and balance
-        const expenses = document.getElementById('expenses');
-        const expensesText = expenses.innerText;
-        expenses.innerText = expenseTotal;
-    
-        // cutting the expense from income and show it in Balance
-        const balance = document.getElementById('balance');
-        const balanceText = balance.innerHTML;
-        balance.innerHTML = incomeNumber - expenseTotal;
+ 
+    // calculating total expense and balance
+    const expenses = document.getElementById('expenses');
+    expenses.innerText = expenseTotal;
+ 
+    // cutting the expense from income and show it in Balance
+    const balance = document.getElementById('balance');
+    balance.innerHTML = incomeNumber - expenseTotal;
 }
-
+ 
 document.getElementById('calc-btn').addEventListener('click', function(){
     commonFunction()
 })
-
+ 
 document.getElementById('save-btn').addEventListener('click', function(){
-        // get income
-        const incomeInput = document.getElementById('income-input').value;
-        const incomeNumber = parseFloat(incomeInput);
-
+    // get income
+    const incomeValue = parseFloat(document.getElementById('income-input').value);
+    const totalExpenses = parseFloat(document.getElementById('expenses').innerText);
+ 
     const saveInput = document.getElementById('save-input');
-    const saveInputValue = saveInput.value;
-
+ 
+    if(saveInput.value == '' || isNaN(saveInput.value) || saveInput.value < 0){
+        alert('Save input value is wrong.');
+        return;
+    }
+ 
+    const saveInputValue = parseFloat(saveInput.value);
+    const saveAmount = (incomeValue * saveInputValue) / 100;
+    // console.log(saveAmount);
+ 
+    const balanceBox = document.getElementById('balance');
+    const balanceAmount = parseFloat(balanceBox.innerText);
+    // console.log(balanceAmount);
+ 
+    if(saveAmount > balanceAmount){
+        alert(`Can not save more than balance`);
+        return;
+    }
+ 
     const savingAmount = document.getElementById('saving-amount');
-    const savingAmountValue = savingAmount.innerText;
-    savingAmount.innerText = (incomeNumber * saveInputValue) / 100;
-
+    savingAmount.innerText = saveAmount;
+ 
     const remaining = document.getElementById('remaining');
-    const remainingValue = remaining.innerText;
-
-    commonFunction()
-    remaining.innerText = balance.innerHTML - savingAmount.innerHTML;
+    remaining.innerText = incomeValue - (totalExpenses + saveAmount);
 })
-
-function errorHandle(){
-    const incomeInput = document.getElementById('income-input').value;
-    const foodInput = document.getElementById('food-input').value;
-    const rentInput = document.getElementById('rent-input').value;
-    const clothsInput = document.getElementById('cloths-input').value;
-    const saveInput = document.getElementById('save-input').value;
-
-    const passMsg = document.getElementById('pass');
-    const failMsg = document.getElementById('fail');
-
-    if((incomeInput > 0 && foodInput > 0 && rentInput > 0 && clothsInput> 0 && saveInput > 0)  && (incomeInput != String && foodInput != String && rentInput != String && clothsInput != String && saveInput != String)){
-        passMsg.style.display = 'block';
-        failMsg.style.display = 'none';
-        // console.log('yeah');
-    }
-    else{
-        passMsg.style.display = 'none';
-        failMsg.style.display = 'block';
-        // console.log('nah');
-    }
-}
-
-
-//  && (incomeInput != String && foodInput != String && rentInput != String && clothsInput != String && saveInput != String)
