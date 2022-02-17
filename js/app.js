38559e8be3f1e11657926c111baea74f9d5a8d71
shufol islam <shufolislam1255@gmail.com>
2022-02-17 19:34:08 +0600

    almost done

--- js/app.js
+++ js/app.js
@@ -46,3 +46,28 @@ document.getElementById('save-btn').addEventListener('click', function(){
     commonFunction()
     remaining.innerText = balance.innerHTML - savingAmount.innerHTML;
 })
+
+function errorHandle(){
+    const incomeInput = document.getElementById('income-input').value;
+    const foodInput = document.getElementById('food-input').value;
+    const rentInput = document.getElementById('rent-input').value;
+    const clothsInput = document.getElementById('cloths-input').value;
+    const saveInput = document.getElementById('save-input').value;
+
+    const passMsg = document.getElementById('pass');
+    const failMsg = document.getElementById('fail');
+
+    if((incomeInput > 0 && foodInput > 0 && rentInput > 0 && clothsInput> 0 && saveInput > 0)  && (incomeInput != String && foodInput != String && rentInput != String && clothsInput != String && saveInput != String)){
+        passMsg.style.display = 'block';
+        failMsg.style.display = 'none';
+        // console.log('yeah');
+    }
+    else{
+        passMsg.style.display = 'none';
+        failMsg.style.display = 'block';
+        // console.log('nah');
+    }
+}
+
+
+//  && (incomeInput != String && foodInput != String && rentInput != String && clothsInput != String && saveInput != String)
